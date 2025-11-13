a08a780b93f338d424d6969d9bee440b566e1699
Alexandre C. Andriani Jr. <alexandre.andriani.jr@gmail.com>
2025-11-13 14:03:40 -0300

    fix(security): Rotate and remove hardcoded DB password

--- src/main/java/com/mycompany/a3_2025_2_gqs/DAO/Conexao.java
+++ src/main/java/com/mycompany/a3_2025_2_gqs/DAO/Conexao.java
@@ -1,33 +1,22 @@
 package com.mycompany.a3_2025_2_gqs.DAO;
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-import java.sql.*;
-
-/**
- *
- * @author Guilherme
- */
 public class Conexao {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/dbtooltracker";
+    private static final String USER = "ghff13";
+    private static final String PASSWORD = "NEW_SECURE_PASSWORD"; 
 
     public Connection getConnection() {
         Connection conn = null;
         try {
-            
-    conn = DriverManager.getConnection(
-        "jdbc:mysql://localhost:3306/dbtooltracker",
-        "ghff13",
-        "LJL!(D`ooACL<3P>6?^9u:nPl\\lgc'@jGUJo0cDLfM\\c--\\,V.2Nkb;KaK3*X^pm"
-    );
-
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException ex) {
             System.out.println("Erro ao conectar ao banco de dados: " + ex.getMessage());
-
         }
-
         return conn;
     }
 }
