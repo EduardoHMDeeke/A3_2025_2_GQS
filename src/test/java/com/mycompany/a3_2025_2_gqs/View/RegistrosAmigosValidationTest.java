4bfe9160479f2b0e8143673ddf29e192a2b00a19
Ed <eduardohmdeeke@gmail.com>
2025-11-16 19:10:12 -0300

    [Fix] Fixed mistake I did

--- src/test/java/com/mycompany/a3_2025_2_gqs/View/RegistrosAmigosValidationTest.java
+++ src/test/java/com/mycompany/a3_2025_2_gqs/View/RegistrosAmigosValidationTest.java
@@ -13,9 +13,14 @@ import java.awt.GraphicsEnvironment;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("RegistrosAmigos Validation Tests")
+@EnabledIf("isGuiTestSupported")
 class RegistrosAmigosValidationTest {
 
     private RegistrosAmigos view;
+
+    private static boolean isGuiTestSupported() {
+        return !GraphicsEnvironment.isHeadless();
+    }
 
     @BeforeEach
     void setUp() {
