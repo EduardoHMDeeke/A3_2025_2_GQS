4171602792d92f68de77256dfcd09068b757c822
ryanpzr <ryanpereiralimads@gmail.com>
2025-11-17 01:37:47 -0300

    test testObtemNumComTextoComNumeros

---
+++ src/test/java/com/mycompany/a3_2025_2_gqs/Util/UtilTest.java
@@ -0,0 +1,23 @@
+package com.mycompany.a3_2025_2_gqs.Util;
+
+import org.junit.jupiter.api.Test;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Calendar;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Testes para a classe Util
+ */
+public class UtilTest {
+
+    @Test
+    void testObtemNumComTextoComNumeros() {
+        int resultado = Util.obtemNum("abc123def456");
+        assertEquals(123456, resultado);
+    }
+
+
+}
+
